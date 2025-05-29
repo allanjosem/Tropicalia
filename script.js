37000502286fa6c37ad82b423c40ced818902c85
allanjosem <168454546+allanjosem@users.noreply.github.com>
2025-05-29 10:48:02 -0300

    29\05

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function (){
+    const botaoDeAcessibilidade= document.getElementById('botao-acessibilidade')
+    const opcaosDeAcessibilidade= document.getElementById('opcao-acessibilidade')
+botaodeacessibilidade
+
+})
